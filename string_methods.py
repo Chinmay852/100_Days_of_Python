d95f9b5ca84df9457015c8e3e686cc03841dfd78
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-09 09:11:15 +0530

    Learn String Methods in Python

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+# Strings are immutable
+
+c="chinmay satam"
+# print(len(c))
+# print(c.upper())
+# print(c.lower())
+# print(c.replace("chinmay","King"))
+# print(c.split())
+# print(c.capitalize())
+# print(c.center(50))
+# print(c.count("chinmay"))
+# print(c.istitle())
+# print(c.swapcase())
+print(c.title())
