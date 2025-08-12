a38e901dd5db6b3c9b044548060982c5a40a00d3
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-12 11:43:38 +0530

    Loops Practice

---
+++ loops_practice.py
@@ -0,0 +1,19 @@
+#while loop
+i = 1
+while i<=10:
+    print("2 X", i, "=", 2*i)
+    i = i+1
+    
+    
+    
+#for loop   
+for i in range (1,11):
+    print("2 X", i, "=", 2*i)
+
+
+a=int(input("Enter Starting number: "))
+b=int(input("Enter Ending number: "))
+
+for i in range(1,b+1):
+    print(a,"x",i, "=", a*i)
+
