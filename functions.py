67252b3a57f6c102aacc527c10d71602b98eedaa
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-15 14:37:53 +0530

    Functions in Python

---
+++ functions.py
@@ -0,0 +1,14 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+    
+
+def isGreater(a,b):
+    if(a>b):
+        print("First Number is Greater")
+    else:
+        print("Second Number is greater or equal as first")
+
+c=6;d=5;
+calculateGmean(c,d)
+isGreater(c,d)
