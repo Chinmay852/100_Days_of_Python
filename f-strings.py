8018d1b83cdadb69da71fa9b4b4aec66db1dc11d
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-19 14:03:59 +0530

    f-strings in Python

---
+++ f-strings.py
@@ -0,0 +1,9 @@
+letter = "Hey my name is {} and I am from {}"
+name = "Chinmay"
+country = "India"
+
+print(letter.format(name , country))
+
+print(f"Hey my name is {{name}} and I am from {{country}}")
+
+# print(f"{2*60}")
