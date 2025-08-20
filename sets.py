0d3f241c466c8d42274572f6cccaecc828193c36
Chinmay Satam <chinmaysatam809@gmail.com>
2025-08-20 12:16:45 +0530

    Sets in Python

---
+++ sets.py
@@ -0,0 +1,11 @@
+set1 = {2,5,6,7,8,2}
+print(set1) 
+
+# set do not maintain order in output + it does not allow duplicate value
+info ={"carla", 19, False, 5.9, 19} 
+print(info)
+for value in info:
+    print(value)
+
+chinmay = set()
+print(type(chinmay))
