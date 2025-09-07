4a714e4361868ea2b264a694ae7ff28ace35e470
Chinmay Satam <chinmaysatam809@gmail.com>
2025-09-07 13:42:44 +0530

    Raise_Custom_Error

---
+++ raise_error.py
@@ -0,0 +1,10 @@
+# a=int(input("Enter a number: "))
+# if(a<5 or a>9):
+#     raise ValueError("Value should be between 5 and 9")
+# print("Code Exceuted")
+
+b=input("Enter a letter: ")
+if(b != "quit"):
+    raise ValueError("No other letters other than quit are allowed")
+print("Success")
+
